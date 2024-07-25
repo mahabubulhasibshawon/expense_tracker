a75b728bf2e3144132b5ba703cd4ee8d7bc8853a
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-07-25 23:30:22 +0600

    update expense.dart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,14 +1,18 @@
+import 'package:uuid/uuid.dart';
 
+final uuid = const Uuid();
 
 class Expense {
   Expense({
-    required this.id,
+    // required this.id,
     required this.title,
     required this.amount,
     required this.date,
-})
+    required this.category
+}): id = uuid.v4();
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final String category;
 }
