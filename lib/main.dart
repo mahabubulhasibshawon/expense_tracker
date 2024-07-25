afcc4f05e07c0a25a862da8ac4fba7aba332490c
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-07-25 22:15:16 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:expanse_tracker/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
+    ),
+  );
+}
