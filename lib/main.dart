321975e36d5ee28f4ce589dc240e8279b968ddc1
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-09-12 00:11:06 +0600

    dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,25 @@ import 'package:flutter/material.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark, seedColor: Color.fromARGB(255, 5, 99, 125));
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
+              backgroundColor: kDarkColorScheme.primaryContainer),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
@@ -28,6 +42,7 @@ void main() {
                   fontSize: 17),
             ),
       ),
+      themeMode: ThemeMode.system,
       home: Expenses(),
     ),
   );
